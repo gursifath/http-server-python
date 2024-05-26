1e1a50bd406527cfce8993a08abd0892766299e1
Gursifath Bhasin <gursifath@users.noreply.github.com>
2024-05-26 02:17:13 -0400

    read header

--- app/main.py
+++ app/main.py
@@ -12,16 +12,22 @@ def main():
     client_socket, _ = server_socket.accept() # wait for client
 
     request = client_socket.recv(1024).decode()
+    success = "HTTP/1.1 200 OK\r\n"
+    content_type = "Content-Type: text/plain\r\n"
     path = request.split(" ")
-    if "echo" not in request.lower():
+
+    if "echo" in request.lower():
+        body = path[1].split("/")[-1]
+        response = f"{success}{content_type}Content-Length: {len(body)}\r\n\r\n{body}"
+    elif "user-agent" in request.lower():
+        path = request.split("\r\n")
+        user_agent = path[2].split(" ")[-1]
+        response = f"{success}{content_type}Content-Length: {len(user_agent)}\r\n\r\n{user_agent}"
+    else:
         if path[1] != "/":
             response = "HTTP/1.1 404 Not Found\r\n\r\n"
         else:
-            response = "HTTP/1.1 200 OK\r\n\r\n"
-    else:
-        body = path[1].split("/")[-1]
-        body_len = len(body)
-        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {body_len}\r\n\r\n{body}"
+            response = f"{success}\r\n"
 
     client_socket.sendall(response.encode())
     client_socket.close
