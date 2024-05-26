f86b23bf8a3457f123ae5539c19ccaa4eb61adf4
Gursifath Bhasin <gursifath@users.noreply.github.com>
2024-05-26 02:00:13 -0400

    respond with body

--- app/main.py
+++ app/main.py
@@ -13,11 +13,18 @@ def main():
 
     request = client_socket.recv(1024).decode()
     path = request.split(" ")
-    if path[1] != "/":
-        response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+    if "echo" not in request.lower():
+        if path[1] != "/":
+            response = "HTTP/1.1 404 Not Found\r\n\r\n"
+        else:
+            response = "HTTP/1.1 200 OK\r\n\r\n"
     else:
-        response = b"HTTP/1.1 200 OK\r\n\r\n"
-    client_socket.sendall(response)
+        body = path[1].split("/")[-1]
+        body_len = len(body)
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {body_len}\r\n\r\n{body}"
+
+    client_socket.sendall(response.encode())
+    client_socket.close
 
 if __name__ == "__main__":
     main()
